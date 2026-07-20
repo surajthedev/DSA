72829b899769c4f2055cc9416f600f181bb14ec9
Suraj Kumar <surajkr2662003@gmail.com>
2026-07-20 13:10:30 +0530

    Add files via upload

---
+++ LeetCode/23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,89 @@
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+# Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+# Example 1:
+
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted linked list:
+# 1->1->2->3->4->4->5->6
+# Example 2:
+
+# Input: lists = []
+# Output: []
+# Example 3:
+
+# Input: lists = [[]]
+# Output: []
+ 
+
+# Constraints:
+
+# k == lists.length
+# 0 <= k <= 104
+# 0 <= lists[i].length <= 500
+# -104 <= lists[i][j] <= 104
+# lists[i] is sorted in ascending order.
+# The sum of lists[i].length will not exceed 104.
+
+# Brute Force:
+class Solution:
+    def mergeKLists(self, lists):
+        arr = []
+
+        # Store all values
+        for head in lists:
+            while head:
+                arr.append(head.val)
+                head = head.next
+
+        # Sort values
+        arr.sort()
+
+        # Create new linked list
+        dummy = ListNode(0)
+        curr = dummy
+
+        for val in arr:
+            curr.next = ListNode(val)
+            curr = curr.next
+
+        return dummy.next
+
+
+
+
+# Optimal Solution:
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists):
+        heap = []
+
+        # Push first node of every list
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode(0)
+        curr = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+
+            curr.next = node
+            curr = curr.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
